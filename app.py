4220632d19f21ffd0f849f549a54af42c14974ca
Yokina <yokina@MacBook-Air-Yokina.local>
2026-08-08 12:19:36 +0400

    Fix command injection vulnerability in app.py

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ def main():
     env_name = os.getenv("APP_ENV", "development")
     print(f"Environment: {env_name}")
 
-    # Небезопасный вызов (Bandit обнаружит B602: subprocess_popen_with_shell_equals_true)
-    user_input = "echo Hello"
-    subprocess.Popen(user_input, shell=True)
+    # Безопасный вызов через список аргументов
+    subprocess.run(["echo", "Hello from secure process!"], check=True)
 
 if __name__ == "__main__":
     main()
